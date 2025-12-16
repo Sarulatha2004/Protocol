fbd33694e039cc5ad1659a66909d088c0dc252e0
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-16 14:45:28 +0530

    Add files via upload

---
+++ APB Protocol/apb_master.v
@@ -0,0 +1,63 @@
+module apb_master(
+ input pclk,
+ input presetn,
+ input r_w,
+ input transfer,
+ input ready,
+ input [7:0]write_paddr,
+ input [7:0]write_data,
+ input [7:0]read_paddr,
+ input [7:0]prdata,
+ output reg psel,
+ output reg penable,
+ output reg pwrite,
+ output reg [7:0]paddr,
+ output reg [7:0]pwrite_data,
+ output reg [7:0]read_data_out
+);
+
+
+reg [1:0]state,next_state;
+parameter IDLE=2'b00;
+parameter SETUP=2'b01;
+parameter ACCESS =2'b10;
+
+always @ (posedge pclk or posedge presetn)
+begin
+	if(!presetn)
+		state <= IDLE;
+	else
+		state <= next_state;
+end
+
+always @ (*) begin
+	case(state)
+		IDLE:
+			next_state = transfer ? SETUP:IDLE;
+		SETUP:
+			next_state = ACCESS;
+		ACCESS:
+			next_state = (ready==0)? ACCESS:(transfer ? SETUP: IDLE);
+	endcase
+end
+
+always @(*) begin
+	if(state == IDLE)
+		psel=0;
+	if(state == ACCESS)
+		penable=1;
+
+        pwrite =1'b0;
+	paddr =8'b0;
+	pwrite_data = 8'b0;
+	read_data_out = 8'b0;
+
+	if(state == SETUP  ||  state == ACCESS)
+	begin
+		pwrite = r_w;
+                pwrite_data = r_w ? write_data:8'b0;
+		paddr = r_w ? write_paddr: read_paddr;
+                read_data_out = r_w ? 8'b0:prdata;
+	end
+end
+endmodule
